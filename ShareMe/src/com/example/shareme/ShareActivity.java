c4324e467f5b9931eb2bff7b129bec73baf36f53
mpmccarthy5 <mpmccarthy5@gmail.com>
2014-02-25 11:41:10 -0500

    Start of project, GUI

---
+++ ShareMe/src/com/example/shareme/ShareActivity.java
@@ -0,0 +1,12 @@
+package com.example.shareme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShareActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sharescreen);
+    }
+}
